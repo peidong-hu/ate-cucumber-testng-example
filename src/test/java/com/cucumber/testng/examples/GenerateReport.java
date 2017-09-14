072a89b4950159d035c5b4e908242732df1a263a
Jacques <wpbxwpbx@gmail.com>
2017-09-13 21:12:26 -0400

    Update cucumber-reporting from 0.6.0->3.10.0

--- src/test/java/com/cucumber/testng/examples/GenerateReport.java
+++ src/test/java/com/cucumber/testng/examples/GenerateReport.java
@@ -1,6 +1,9 @@
 package com.cucumber.testng.examples;
 
+import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.Reportable;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +14,7 @@ import java.util.List;
 public class GenerateReport {
     public static void GenerateMasterthoughtReport(){
         try{
-            String RootDir = System.getProperty("user.dir");
+            /*String RootDir = System.getProperty("user.dir");
             File reportOutputDirectory = new File("target/Masterthought");
             List<String> list = new ArrayList<String>();
             list.add("target/cucumber1.json");
@@ -34,8 +37,35 @@ public class GenerateReport {
             ReportBuilder reportBuilder = new ReportBuilder(list, reportOutputDirectory, pluginUrlPath, buildNumber,
                     buildProject, skippedFails, pendingFails, undefinedFails, missingFails, flashCharts, runWithJenkins,
                     highCharts, parallelTesting);
+            reportBuilder.generateReports();*/
+            
+            File reportOutputDirectory = new File("target/Masterthought");
+            List<String> jsonFiles = new ArrayList<String>();
+            jsonFiles.add("target/cucumber1.json");
+            jsonFiles.add("target/cucumber2.json");
 
-            reportBuilder.generateReports();
+            String buildNumber = "1";
+            String projectName = "cucumber-jvm";
+            boolean runWithJenkins = false;
+            boolean parallelTesting = false;
+
+            Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+            
+            // optional configuration
+            configuration.setParallelTesting(parallelTesting);
+            configuration.setRunWithJenkins(runWithJenkins);
+            configuration.setBuildNumber(buildNumber);
+            
+            // addidtional metadata presented on main page
+            /*configuration.addClassifications("Platform", "Windows");
+            configuration.addClassifications("Browser", "Firefox");
+            configuration.addClassifications("Branch", "release/1.0");*/
+
+            ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+            Reportable result = reportBuilder.generateReports();
+	        // and here validate 'result' to decide what to do
+	        // if report has failed features, undefined steps etc
+            System.out.println("This is Reportable: " + result.toString());
         }catch(Exception e){
             e.printStackTrace();
         }
